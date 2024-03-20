a377d055bec61fad06e40c97b638402919d3dd9e
4l3j4ndr0 <alejandro.casta3447@gmail.com>
2024-03-20 11:29:27 -0500

    :sparkles: Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Import the express module
+const express = require("express");
+
+// Create an instance of express
+const app = express();
+
+// Define the port number
+const PORT = 3000;
+
+// Define a GET endpoint
+app.get("/", (req, res) => {
+  // Send a response with text and emoji when the endpoint is accessed
+  res.send("Cloudcamp API V1 🚀");
+});
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
